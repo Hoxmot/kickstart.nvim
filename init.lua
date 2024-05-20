65e300d5491b6cf7289105478a08144b0de15720
Hoxmot <hoxmot@gmail.com>
2024-05-20 21:25:13 +0200

    Added neo-tree
    Added 'nvim-neo-tree/neo-tree.nvim' for easier navigation through the
    files. Also, added basic config & keemappings. The editor should behave
    like netrw
    
    TODO:
      * Add a mapping to easily open the file browser

--- init.lua
+++ init.lua
@@ -409,6 +409,31 @@ require('lazy').setup({
   },
 
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require('neo-tree').setup {
+        window = {
+          position = 'current',
+          mappings = {
+            ['h'] = 'close_node',
+            ['l'] = 'open',
+          },
+        },
+        filesystem = {
+          hijack_netrw_behavior = 'open_default',
+        },
+      }
+    end,
+  },
+
+  {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
