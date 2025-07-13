f7d942486167ed929d8edf590e70a40a0120c877
Hoxmot <hoxmot@gmail.com>
2025-07-13 14:59:19 +0200

    Added bash lsp & formatter

--- init.lua
+++ init.lua
@@ -679,18 +679,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-        zls = {},
-
+        bashls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -705,6 +694,17 @@ require('lazy').setup({
             },
           },
         },
+        pyright = {},
+        zls = {},
+        -- rust_analyzer = {},
+        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        --
+        -- Some languages (like typescript) have entire language plugins that can be useful:
+        --    https://github.com/pmizio/typescript-tools.nvim
+        --
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
+        --
       }
 
       -- Ensure the servers and tools above are installed
@@ -778,6 +778,7 @@ require('lazy').setup({
         typescript = { 'prettier', 'prettierd', 'eslint_d', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        sh = { 'shfmt' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
