0c0c1bb523e2eaf4aff86f796cf889ee03f3c0b1
Hoxmot <hoxmot@gmail.com>
2025-07-17 23:05:47 +0200

    Moved my custom config to separate file
    This will avoid conlficts with official kickstart.nvim

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.o.relativenumber = true
+-- vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -205,6 +205,8 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+require 'custom.init'
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -253,7 +255,6 @@ require('lazy').setup({
       require('guess-indent').setup {
         on_tab_options = {
           ['expandtab'] = false,
-          ['tabstop'] = 4,
           ['softtabstop'] = 4,
           ['shiftwidth'] = 4,
         },
---
+++ lua/custom/init.lua
@@ -0,0 +1,7 @@
+-- My favourite relative lines
+vim.o.relativenumber = true
+
+-- Setting my preferred tabs
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
